bdec2639bb8df4e05b5af8e1a7fdb5a280c2f912
Tori McFarlin <tori.mcfarlin@greensync.com.au>
2017-10-16 21:05:09 +1100

    Log description when clicking image

--- specs/SweetImage.spec.js
+++ specs/SweetImage.spec.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, TouchableHighlight } from 'react-native';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
 import SweetImage from '../src/components/SweetImage';
 
+const image = 'https://st.depositphotos.com/1801791/1399/i/950/depositphotos_13999421-stock-photo-colorful-spiral-lollipop-lolly-pop.jpg';
+
 describe('SweetImage', () => {
   describe('source', () => {
     it('should render source from uri', () => {
-      const image = 'https://st.depositphotos.com/1801791/1399/i/950/depositphotos_13999421-stock-photo-colorful-spiral-lollipop-lolly-pop.jpg';
-
       const view = shallow(<SweetImage image={image} />);
 
       expect(view.find(Image).props().source).to.eql({ uri: image });
+    });
+  });
+
+  describe('description', () => {
+    let consoleStub;
+
+    beforeEach(() => {
+      consoleStub = sinon.stub(console, 'info');
+    });
+
+    afterEach(() => {
+      console.info.restore();
+    });
+
+    it('should console log description when pressed', () => {
+      const description = 'So long, Candy Boys'
+      const view = shallow(<SweetImage image={image} description={description} />);
+
+      view.find(TouchableHighlight).simulate('press');
+
+      expect(consoleStub).to.have.been.calledWith(description);
     });
   });
 });
--- src/components/SweetImage.js
+++ src/components/SweetImage.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, TouchableHighlight } from 'react-native';
 
 const re = new RegExp("^(http|https)://", "i");
 const imageSource = (image) => re.test(image) ? { uri: image } : image;
 
-export default ({ image, description, style }) => (
-  <View>
-    <Image
-      key={image}
-      style={style}
-      source={imageSource(image)}
-    />
-  </View>
-);
+export default ({ image, description, style }) => {
+  const logDescription = () => console.info(description);
+  return (
+    <TouchableHighlight
+      onPress={logDescription}
+    >
+      <View>
+        <Image
+          key={image}
+          style={style}
+          source={imageSource(image)}
+        />
+      </View>
+    </TouchableHighlight>
+  );
+}
