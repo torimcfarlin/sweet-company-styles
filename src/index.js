9a1a0b376d2857f2a1e1af1f5391b776801c9b06
Tori McFarlin <tori.mcfarlin@greensync.com.au>
2017-10-16 20:43:44 +1100

    Export SweetImage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as SweetText } from "./components/SweetText";
 export { default as SweetTitle } from "./components/SweetTitle";
 export { default as SweetButton } from "./components/SweetButton";
+export { default as SweetImage } from "./components/SweetImage";
 
 export {
   bubbleGum,
